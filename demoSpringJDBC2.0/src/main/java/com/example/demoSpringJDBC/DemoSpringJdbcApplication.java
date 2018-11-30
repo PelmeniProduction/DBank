8b682284c87860280d53d4cc838e711b47cbb143
VologdinI <vologdin2424@gmail.com>
2018-11-30 15:20:27 +0300

    Бла бла

---
+++ demoSpringJDBC2.0/src/main/java/com/example/demoSpringJDBC/DemoSpringJdbcApplication.java
@@ -0,0 +1,12 @@
+package com.example.demoSpringJDBC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringJdbcApplication.class, args);
+	}
+}
