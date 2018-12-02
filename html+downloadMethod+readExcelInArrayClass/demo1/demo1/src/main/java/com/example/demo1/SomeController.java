db38457ba996dba40cbc59c76865d544732e61b8
svetlichnyi.constantine <svetlichnyi-0997@mail.ru>
2018-12-02 21:06:59 +0400

    add the class with methods, that read excel-files

---
+++ html+downloadMethod+readExcelInArrayClass/demo1/demo1/src/main/java/com/example/demo1/SomeController.java
@@ -0,0 +1,29 @@
+package com.example.demo1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+/**
+ * Created by SK
+ **/
+@Controller
+public class SomeController {
+    @GetMapping(value="/")
+    public String start_page(){
+        return "index";
+    }
+
+    @GetMapping(value="/index")
+    public String index_page(){
+        return "index";
+    }
+
+    @GetMapping(value="/prediction")
+    public String prediction_page(){
+        return "prediction";
+    }
+
+    @GetMapping(value="/instruments")
+    public String instruments_page(){
+        return "instruments";
+    }
+}
