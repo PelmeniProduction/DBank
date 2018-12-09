cc1db9e072b0bbff488b9515981af74fef169905
VologdinI <vologdin2424@gmail.com>
2018-12-09 18:18:03 +0300

    DataCrawler воркает

---
+++ 9_12_18/Kano/demo1/src/main/java/com/example/demo1/AnalyticService.java
@@ -0,0 +1,29 @@
+package com.example.demo1;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.util.Collections;
+import java.util.List;
+
+@Component
+public class AnalyticService {
+
+    @Autowired
+    ConnectionManager connectionManager;
+
+    private volatile List<Object> cachedData = Collections.emptyList();
+
+    @Scheduled
+    public void reloadData() throws Exception {
+        Connection connection = connectionManager.getConnection1();
+
+    }
+
+    public List<Object> getCachedData() {
+        return cachedData;
+    }
+}
