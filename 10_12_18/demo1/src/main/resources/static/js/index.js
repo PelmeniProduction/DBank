7822a16293f4561cd7a8c35e868a44cdfd0b29ff
svetlichnyi.constantine <svetlichnyi-0997@mail.ru>
2018-12-10 17:59:27 +0400

    add date range picker

--- 10_12_18/demo1/src/main/resources/static/js/index.js
+++ 10_12_18/demo1/src/main/resources/static/js/index.js
@@ -1,5 +1,4 @@
 var UPDATE_BUTTON = 'update_data';//id кнопки
-var RADIOBUTTON_CLASS = '.custom-control-input';//класс радиобатонов
 var SELECT_CLASS = '.form-control';//класс выпадающего окна
 
 function init() {
@@ -13,25 +12,22 @@ function getData() {
     console.log('function called');
     var Select = document.querySelector(SELECT_CLASS);
     console.log(Select.options[Select.selectedIndex].text);//выводим выбранный пункт выпадающего меню select
-    var RadioButton = document.querySelectorAll(RADIOBUTTON_CLASS);
-    for (var i = 0; i < RadioButton.length; i++) {
-        if (RadioButton[i].type === "radio" && RadioButton[i].checked) {
-            console.log(RadioButton[i].id);//выводим выбранный пункт radiobutton
-            //ajax-запрос-post
-            $.ajax({
-                type: "POST",
-                url: "/update",
-                data: JSON.stringify({"category": Select.options[Select.selectedIndex].text, "period": RadioButton[i].id}),
-                contentType: "application/json; charset=utf-8",
-                dataType: "json",
-                success: function(arrTSD){
-                    alert(arrTSD);
-                    /*var arrForChart = JSON.parse(arrTSD);*///return из SpringBoot
-
-                },
-                failure: function (errMsg) {alert(errMsg);}
-            })
+    console.log($('#DRP1').val());//выводим выбранный промежуток времени
+    //ajax-запрос-post
+    $.ajax({
+        type: "POST",
+        url: "/update",
+        data: JSON.stringify({"category": Select.options[Select.selectedIndex].text, "period": $('#DRP1').val()}),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (arrTSD) {
+            alert(arrTSD);
+            /*var arrForChart = JSON.parse(arrTSD);*///return из SpringBoot
+        },
+        failure: function (errMsg) {
+            alert(errMsg);
         }
-    }
+    })
 }
+
 document.addEventListener('DOMContentLoaded', init);//Ждём пока страница загрузится, затем вызываем функцию init
