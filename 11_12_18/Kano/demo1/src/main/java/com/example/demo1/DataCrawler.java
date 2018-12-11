0f78a5e8ae9eeeb87ce306bb506cec5953604659
VologdinI <vologdin2424@gmail.com>
2018-12-11 16:57:04 +0300

    Back ready

---
+++ 11_12_18/Kano/demo1/src/main/java/com/example/demo1/DataCrawler.java
@@ -0,0 +1,93 @@
+package com.example.demo1;
+
+import com.example.math.DataBase;
+import com.example.math.DownloadClass;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+@Component
+public class DataCrawler {
+
+    @Autowired
+    ConnectionManager connectionManager;
+
+
+    public static ArrayList<TimeSeriesData> arTimeSeriesData;
+
+    @Scheduled(fixedDelay = 1000*60*60*24)
+    public void reloadData() throws Exception {
+        connectionManager= new ConnectionManager();
+        Connection connection = connectionManager.getConnection1();
+        Statement stmt =connection.createStatement();
+        Date dInit= new Date(0001,01,01);//начало времен, для сравнения
+        Date start = new Date(0001,01,01); // DB.getLastUpdated()
+        start= DataBase.getLastUpdated(stmt);
+        if (isSameDate(dInit,start)) {//если в таблице ничего нет
+            //поставим начало времен как старт
+            start = new Date(103,01,01);
+        }
+        Date end = new Date();
+        //Date end = new Date(118,10,29);
+        if(isSameDate(start,end)) {//если сегодня уже обновлялось
+            connection.close();
+        }else {
+            start=subtractFromDate(-1,start);//добавим день, иначе один день повторится
+            String url = makeUrl(start, end);
+            arTimeSeriesData = DownloadClass.downloadUsingStream(url);
+            DataBase.fillTable(connection, arTimeSeriesData);
+            connection.commit();
+            connection.close();
+        }
+    }
+    public boolean isSameDate(Date d0, Date d1){
+        //d0=subtractFromDate(-1,new Date());//добавим 1
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(d0);
+        int yearStart = cal.get(Calendar.YEAR);
+        int monthStart = cal.get(Calendar.MONTH);//sql считает месяцы с 0, а сайт с 1
+        int dayStart = cal.get(Calendar.DAY_OF_MONTH);
+        cal.setTime(d1);
+        int yearEnd = cal.get(Calendar.YEAR);
+        int monthEnd = cal.get(Calendar.MONTH);//sql считает месяцы с 0, а сайт с 1
+        int dayEnd = cal.get(Calendar.DAY_OF_MONTH);
+        boolean b =(yearStart==yearEnd)&&(monthStart==monthEnd)&&(dayStart==dayEnd);
+        return b;
+    }
+    //сделаем урл
+    public String makeUrl(Date dStart,Date dEnd){
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(dStart);
+        int yearStart = cal.get(Calendar.YEAR);
+        int monthStart = cal.get(Calendar.MONTH)+1;//sql считает месяцы с 0, а сайт с 1
+        int dayStart = cal.get(Calendar.DAY_OF_MONTH);
+        cal.setTime(dEnd);
+        int yearEnd = cal.get(Calendar.YEAR);
+        int monthEnd = cal.get(Calendar.MONTH)+1;//sql считает месяцы с 0, а сайт с 1
+        int dayEnd = cal.get(Calendar.DAY_OF_MONTH);
+        String url = "http://gold.investfunds.ru/indicators/export_to_xls.php?id=224&start_day="+makeSecondSign(dayStart)+"&start_month="+makeSecondSign(monthStart)+"&start_year="+yearStart+"&finish_day="+makeSecondSign(dayEnd)+"&finish_month="+makeSecondSign(monthEnd)+"&finish_year="+yearEnd;
+        return url;
+    }
+    public String makeSecondSign(int i){
+        String str="";
+        if(i/10==0){
+            str+="0";
+        }
+        str+=i;
+        return str;
+    }
+    //вычесть из текущей даты несколько дней
+    public static Date subtractFromDate(int iDays,Date date){
+        Calendar instance = Calendar.getInstance();
+        instance.setTime(date); //устанавливаем дату, с которой будет производить операции
+        instance.add(Calendar.DAY_OF_MONTH, -iDays);
+        Date newDate = instance.getTime(); // получаем измененную дату
+        return newDate;
+    }
+}
---
+++ 11_12_18/Kano/demo1/src/main/java/com/example/math/DataBase.java
@@ -0,0 +1,149 @@
+package com.example.math;
+
+
+import com.example.demo1.TimeSeriesData;
+import sun.misc.FloatingDecimal;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * Created by Kano on 09.11.2018.
+ */
+public class DataBase {
+    //создать таблицу
+    static void crateTable(Statement stmt){
+        try{
+            String sql = "CREATE TABLE GOLD " +
+                    "(ID INT PRIMARY KEY     NOT NULL," +
+                    " COAST           REAL    NOT NULL, " +
+                    " DAYS            DATE     NOT NULL)";
+            stmt.executeUpdate(sql);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+            //System.exit(0); убить процесс
+        }
+    }
+    //удалить таблицу
+    static void dropTable(Statement stmt) {
+        try{
+            String sql = "DROP TABLE GOLD ";
+            stmt.executeUpdate(sql);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+        }
+    }
+    //вывести на консоль базу
+    static void printTable(Statement stmt) {
+        try{
+            ResultSet rs = stmt.executeQuery("SELECT * FROM GOLD;");
+            while (rs.next()) {
+                int id = rs.getInt("ID");
+                float coast = rs.getFloat("COAST");
+                Date days = rs.getDate("DAYS");
+                System.out.println("ID = " + id + " COAST = " + coast + " DATE= " + days);
+            }
+            System.out.println("Print successfully");
+            rs.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+        }
+    }
+    static void deleteAllRecords(Statement stmt){
+        try{
+            String sql;
+            sql="DELETE from GOLD ;";
+            stmt.execute(sql);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+        }
+    }
+    //заполнить базу
+    public static void fillTable(Connection c, ArrayList<TimeSeriesData> arTsd ){//saveAll
+        try{
+            ArrayList<Double> ar = new ArrayList<>();
+            for (int i = 0; i < arTsd.size(); i++) {
+                ar.add(arTsd.get(i).getPrice());
+            }
+            String sqlPrepSt;
+            for (int i = 0; i < arTsd.size(); i++) {
+                java.sql.Date sDate = new java.sql.Date(arTsd.get(i).getDate().getTime());
+                sqlPrepSt = "INSERT INTO GOLD (COAST,DAYS) "
+                        + "VALUES (?, ? );";
+                PreparedStatement preparedStatement = c.prepareStatement(sqlPrepSt);
+                //preparedStatement.setInt(1,i);
+                preparedStatement.setDouble(1,arTsd.get(i).getPrice());
+                preparedStatement.setDate(2,sDate);
+                preparedStatement.executeUpdate();
+                //stmt.executeUpdate(sqlPrepSt);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+        }
+    }
+    public static Date getLastUpdated(Statement stmt){
+        Date dLastDate= new Date(0001,01,01);
+        Date dCurrent;
+        try{
+            ResultSet rs = stmt.executeQuery("SELECT * FROM GOLD ;");//так не очень, если будет время поменять
+            while (rs.next()) {
+                int k=0;
+                dCurrent= rs.getDate("DAYS");
+                if(dCurrent==new Date(2018,10,23))
+                    k=3;
+                if(dCurrent.getTime()>dLastDate.getTime())
+                    dLastDate=dCurrent;
+            }
+            rs.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+        }
+        return dLastDate;
+    }
+    public static ArrayList<TimeSeriesData> getListFromDate(Connection c,Statement stmt, Date start,Date end){
+        ArrayList<TimeSeriesData> arTsd = new ArrayList<>();
+        /**/
+
+        try{
+            java.sql.Date sStart = new java.sql.Date(start.getTime());
+            java.sql.Date sEnd = new java.sql.Date(end.getTime());
+            /*String sqlPrepSt;
+            sqlPrepSt = "SELECT * FROM gold WHERE days>? AND days<?;";
+            PreparedStatement preparedStatement = c.prepareStatement(sqlPrepSt);
+            preparedStatement.setDouble(1,sStart);
+            preparedStatement.setDate(2,sEnd);
+            preparedStatement.executeUpdate();*/
+            ResultSet rs = stmt.executeQuery("SELECT * FROM GOLD WHERE days>='"+sStart+"'AND days<='"+sEnd+"'");
+            while (rs.next()) {
+                int id = rs.getInt("ID");
+                float coast = rs.getFloat("COAST");
+                Date days = rs.getDate("DAYS");
+                double dCoast=(double)coast;
+                arTsd.add(new TimeSeriesData("gold",days,dCoast));
+                System.out.println("ID = " + id + " COAST = " + coast + " DATE= " + days);
+            }
+            rs.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+        }
+        return arTsd;
+    }
+
+    static boolean lastPeriodLess20(int iPeriod,ArrayList<Double> arD){
+        boolean b=false;
+        if((arD.size()-(iPeriod+1)*20)<20)
+            b=true;
+        return b;
+    }
+}
